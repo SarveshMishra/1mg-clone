74c54040b6e0008f207f5d2d6ea8d575e36996e5
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-03-04 03:12:14 +0530

    added new content in ask doctor

--- js/askdoctor.js
+++ js/askdoctor.js
@@ -8,5 +8,28 @@ document.querySelector(".header-sec-1").innerHTML = await topHeader();
 
 // Body section
 
+  var slides = document.getElementsByClassName("h-customer-slide");
+  var dots = document.getElementsByClassName("dot");
+    var prevDot='';
+    var prevSlide='';
+for(let i=0;i<dots.length;i++){
+    
+    console.log(dots[i].className)
+    dots[i].addEventListener('click',function(){
+
+        if(prevDot.className=='dot dot-active' && prevSlide.style.display=='block'){
+            prevDot.className=prevDot.className.replace('dot-active',"");
+            prevSlide.style.display='none';
+            
+        }
+        dots[i].className="dot dot-active";
+        slides[i].style.display='block';
+        prevSlide=slides[i];
+        prevDot=dots[i]
+        
+    });
+    
+}
+
 // Footer section
 document.querySelector(".footer").innerHTML = await footer();
