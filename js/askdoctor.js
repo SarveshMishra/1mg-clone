d92fa6825c9b47c65ccf8d58825798ad04a5426b
Sarvesh Mishra <officialsmishra@gmail.com>
2022-03-05 11:48:16 +0530

    Login and signup button added to every page

--- js/askdoctor.js
+++ js/askdoctor.js
@@ -1,34 +1,44 @@
 import { topHeader } from "../components/header/navbar.js";
-import showList from "../components/header/index.js";
 import changeLinkColor from "../scripts/changeLinkColor.js";
 import footer from "../components/footer/footer.js";
+import { login, signup } from "../components/login/login.js";
+import {
+	loginOn,
+	signupOn,
+	loginOff,
+	signupOff,
+} from "../scripts/loginSignup.js";
 
 // Navbar Import section
 document.querySelector(".header-sec-1").innerHTML = await topHeader();
-
+document.querySelector(".login-overlay").innerHTML = await login();
+document.querySelector(".signup-overlay").innerHTML = await signup();
+window.loginOn = loginOn;
+window.signupOn = signupOn;
+window.loginOff = loginOff;
+window.signupOff = signupOff;
 // Body section
+changeLinkColor();
 
-  var slides = document.getElementsByClassName("h-customer-slide");
-  var dots = document.getElementsByClassName("dot");
-    var prevDot='';
-    var prevSlide='';
-for(let i=0;i<dots.length;i++){
-    
-    console.log(dots[i].className)
-    dots[i].addEventListener('click',function(){
-
-        if(prevDot.className=='dot dot-active' && prevSlide.style.display=='block'){
-            prevDot.className=prevDot.className.replace('dot-active',"");
-            prevSlide.style.display='none';
-            
-        }
-        dots[i].className="dot dot-active";
-        slides[i].style.display='block';
-        prevSlide=slides[i];
-        prevDot=dots[i]
-        
-    });
-    
+var slides = document.getElementsByClassName("h-customer-slide");
+var dots = document.getElementsByClassName("dot");
+var prevDot = "";
+var prevSlide = "";
+for (let i = 0; i < dots.length; i++) {
+	console.log(dots[i].className);
+	dots[i].addEventListener("click", function () {
+		if (
+			prevDot.className == "dot dot-active" &&
+			prevSlide.style.display == "block"
+		) {
+			prevDot.className = prevDot.className.replace("dot-active", "");
+			prevSlide.style.display = "none";
+		}
+		dots[i].className = "dot dot-active";
+		slides[i].style.display = "block";
+		prevSlide = slides[i];
+		prevDot = dots[i];
+	});
 }
 
 // Footer section
--- js/covid-19.js
+++ js/covid-19.js
@@ -2,24 +2,36 @@ import { topHeader, searchBox, links } from "../components/header/navbar.js";
 import showList from "../components/header/index.js";
 import changeLinkColor from "../scripts/changeLinkColor.js";
 import footer from "../components/footer/footer.js";
+import { login, signup } from "../components/login/login.js";
+import {
+	loginOn,
+	signupOn,
+	loginOff,
+	signupOff,
+} from "../scripts/loginSignup.js";
 
 // Navbar Import section
 document.querySelector(".header-sec-1").innerHTML = await topHeader();
 document.querySelector(".search-box").innerHTML = await searchBox();
-
+document.querySelector(".login-overlay").innerHTML = await login();
+document.querySelector(".signup-overlay").innerHTML = await signup();
+window.loginOn = loginOn;
+window.signupOn = signupOn;
+window.loginOff = loginOff;
+window.signupOff = signupOff;
 window.hide = hide;
-changeLinkColor()
+changeLinkColor();
 document.getElementById("location").addEventListener("click", showList);
 
 function hide() {
-  let container = document.querySelector(".location-selection ");
-  document.addEventListener("mouseup", function (e) {
-    if (!container.contains(e.target)) {
-      container.style.visibility = "hidden";
-    }
-  });
+	let container = document.querySelector(".location-selection ");
+	document.addEventListener("mouseup", function (e) {
+		if (!container.contains(e.target)) {
+			container.style.visibility = "hidden";
+		}
+	});
 }
 
 // Body section
 // Footer section
-document.querySelector(".footer").innerHTML= await footer();
+document.querySelector(".footer").innerHTML = await footer();
